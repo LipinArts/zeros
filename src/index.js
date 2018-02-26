b261d6208929a12c10d74e5da348a4512c3e0ae2
Artsiom Lipin <lipinartsiom@gmail.com>
2018-02-26 20:05:09 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+	let pow = 2;
+	let zerosCount = 0;
+	for (let i = 1; i < pow; i++) {
+		let currNum = Math.floor(number / Math.pow(5, [i]));
+		zerosCount += currNum;
+		if (currNum <= 1) {
+			return zerosCount;
+		} else {
+			pow += i;
+		}
+	}
+};
